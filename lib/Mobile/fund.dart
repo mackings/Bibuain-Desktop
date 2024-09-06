322fba60f3a2506b9527dce988b55cf556734576
mackingsley <kingsleyudoma2018@gmail.com>
2024-09-06 07:58:34 +0100

    verified

--- lib/Mobile/fund.dart
+++ lib/Mobile/fund.dart
@@ -11,11 +11,14 @@ class Fund extends StatefulWidget {
 }
 
 class _FundState extends State<Fund> {
-
-  final TextEditingController _accountNumberController = TextEditingController();
+  final TextEditingController _accountNumberController =
+      TextEditingController();
   String? selectedBankName;
   String? selectedBankCode;
   String? selectedBankLogo;
+  String? accountName; // To store the account holder name
+  bool isLoading = false; // To manage the progress indicator state
+  bool showAccountName = false; // To toggle the display of account name
 
   Future<void> showBankSelectionSheet(BuildContext context) async {
     final banks = await fetchBanks(); // Fetch banks data
@@ -28,9 +31,9 @@ class _FundState extends State<Fund> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Select a Bank',),
+              Text('Select a Bank'),
               SizedBox(height: 10),
-              Text('Popular Banks',),
+              Text('Popular Banks'),
               SizedBox(
                 height: 100,
                 child: ListView(
@@ -56,7 +59,7 @@ class _FundState extends State<Fund> {
                 ),
               ),
               SizedBox(height: 10),
-              Text('All Banks',),
+              Text('All Banks'),
               Expanded(
                 child: ListView.builder(
                   itemCount: banks.length,
@@ -98,6 +101,51 @@ class _FundState extends State<Fund> {
     });
   }
 
+  Future<void> verifyAccountDetails(
+      String accountNumber, String bankCode) async {
+    setState(() {
+      isLoading = true; // Show progress indicator
+      showAccountName = false; // Hide the account name temporarily
+    });
+
+    final response = await http.get(
+      Uri.parse(
+          'https://server-eight-beige.vercel.app/api/wallet/generateBankDetails/$accountNumber/$bankCode'),
+    );
+    print("Acc $accountNumber");
+    print("Code $bankCode");
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print(response.body);
+      setState(() {
+        accountName =
+            data['data']['account_name']; // Assuming the API returns 'accountName'
+        showAccountName = true; // Show the account name
+      });
+    } else {
+      print(response.body);
+      setState(() {
+        accountName = 'Verification failed';
+        showAccountName = true;
+      });
+    }
+
+    setState(() {
+      isLoading = false; // Hide progress indicator
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _accountNumberController.addListener(() {
+      if (_accountNumberController.text.length == 10 &&
+          selectedBankCode != null) {
+        verifyAccountDetails(_accountNumberController.text, selectedBankCode!);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -107,17 +155,23 @@ class _FundState extends State<Fund> {
           style: GoogleFonts.montserrat(fontWeight: FontWeight.w700),
         ),
         centerTitle: true,
+        bottom: isLoading
+            ? const PreferredSize(
+                preferredSize: Size(double.infinity, 4.0),
+                child: LinearProgressIndicator(),
+              )
+            : null, 
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Bank',style: GoogleFonts.montserrat(fontWeight: FontWeight.w700),),
+                Text('Bank',
+                    style: GoogleFonts.montserrat(fontWeight: FontWeight.w700)),
                 Container(
                   child: TextFormField(
                     readOnly: true,
@@ -142,16 +196,19 @@ class _FundState extends State<Fund> {
                 ),
               ],
             ),
+
             SizedBox(height: 20),
-            
+
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Account Number',style: GoogleFonts.montserrat(fontWeight: FontWeight.w700),),
-
+                Text('Account Number',
+                    style: GoogleFonts.montserrat(fontWeight: FontWeight.w700)),
                 Container(
                   child: TextFormField(
                     controller: _accountNumberController,
+                    keyboardType: TextInputType.number,
+                    //maxLength: 10,
                     decoration: InputDecoration(
                       hintText: 'Enter account number',
                       border: OutlineInputBorder(),
@@ -160,6 +217,15 @@ class _FundState extends State<Fund> {
                 ),
               ],
             ),
+            SizedBox(height: 10),
+            if (showAccountName)
+              Row(
+                children: [
+                  Icon(Icons.check_circle,color: Colors.green,),SizedBox(width: 5,),
+                  Text('$accountName',
+                      style: GoogleFonts.montserrat()),
+                ],
+              ),
           ],
         ),
       ),
